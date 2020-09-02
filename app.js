3af5234956acacd36c7f29d038e4abca625ad7d0
Diego Turcios <diegoturciostc@gmail.com>
2020-09-02 11:06:56 -0600

    Test purpose

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 
 // socket connection
 io.on('connection', (socket) => {
-	console.log("Demo - A user connected!");
+	console.log("Demo - A user connected Working!");
 
 	// executes when a user starts typing
 	socket.on('userTyping', (username) => {
